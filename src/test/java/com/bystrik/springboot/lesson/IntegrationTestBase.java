8f9a4a5888020128bf656432c80e6a1304ffb224
Marina Bystrik <O9Oruo1A>
2021-06-09 16:08:50 +0300

    Test containers. Использование

--- src/test/java/com/bystrik/springboot/lesson/IntegrationTestBase.java
+++ src/test/java/com/bystrik/springboot/lesson/IntegrationTestBase.java
@@ -1,12 +1,22 @@
 package com.bystrik.springboot.lesson;
 
+import com.bystrik.springboot.lesson.initializer.Postgres;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 @ActiveProfiles("test")
 @SpringBootTest
 @Transactional
+@ContextConfiguration(initializers = {
+        Postgres.Initializer.class
+})
 public abstract class IntegrationTestBase {
 
+    @BeforeAll
+    static void init(){
+        Postgres.container.start();
+    }
 }
